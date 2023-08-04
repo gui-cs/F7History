be421a3c11edcdee28e6d4e88515f1cebf764fad
Tig Kindel <tig@users.noreply.github.com>
2023-08-04 15:35:52 -0600

    tweak gitversion2

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,10 @@
+$BUILDCOUNT = $BUILDCOUNT + 1
 $ModuleName = "F7History"
 $ModulePath = "./Output"
-rm $ModulePath -Recurse -Force -ErrorAction SilentlyContinue
 $ModuleVersion = dotnet-gitversion /showvariable AssemblySemFileVer
+"Building $ModuleName version $ModuleVersion to $ModulePath"
+
+rm $ModulePath -Recurse -Force -ErrorAction SilentlyContinue
 Build-Module -SemVer $ModuleVersion 
 $ModulePackage = "${ModulePath}" 
 Publish-Module -Path $ModulePackage -Repository 'local'
